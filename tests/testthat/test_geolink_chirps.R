ede9ae013d4695ffd4aea99a8270a2e0361022c7
Chase Young <142842450+Chaser263@users.noreply.github.com>
2023-12-06 11:50:00 -0500

    Updated Geolink_Chirps Tests

--- tests/testthat/test_geolink_chirps.R
+++ tests/testthat/test_geolink_chirps.R
@@ -3,8 +3,8 @@ data("hhgeo_dt")
 data("shp_dt")
 
 #Step 2: load the monthly data from Chirps:
-print("getting testing data from CHIRPS")
-geolink_chirps(time_unit = "month",
+testthat_print("getting testing data from CHIRPS")
+df1<- geolink_chirps(time_unit = "month",
                      start_date = "2020-01-01",
                      end_date = "2020-03-01",
                      shp_dt = shp_dt[shp_dt$ADM1_PCODE == "NG001", ],
@@ -21,48 +21,47 @@ df2<- geolink_chirps(time_unit = "annual",
                      survey_dt = hhgeo_dt,
                      extract_fun = "mean")
 
-#Step 4: Run Tests for Expected Output
-#check shp_dt was correctly loaded
-#Variables in shp_dt: ADM2P_CODE for Arochukwu is NG001003 in Abia
+#Step 4: run tests for expected output for monthly output
+testthat_print("checking for expected output")
+#testing if non-numeric variables are equal
+test_that("testing data is correctly merged", {
+  expect_equal(df1$ADM2_PCODE[61], "NG001014")
+  expect_equal(df1$ADM2_EN[627], "Ugwunagbo")
+  expect_equal(df1$ADM1_EN[6], "Abia")
 
-#Hhgeo_dt:
-##for HHID 19010: dist-pop center= 1
-# for 19104: Pop density= 10700
-# for 10010, NVDI Max= .540
-
-#check if chirps data is added correctly: where is Chirps data added back in?
-
-#mean or rainfall for chirps variable
-
-
-
-
-#Step 5: Create pathways for possible errors
-
-
-
-test_that("sample data is correctly loaded", {
-  expect_equal(hhgeo_dt[1:1]$hhid,10001)
-  expect_equal(shp_dt$ADM0_PCODE[1],"NG")
 })
 
-test_that("sample works for R-user)", {
-  expect_equal(df$dist_road2[1], 1.1)
-  expect_equal(df$af_bio_13[2], 320)
-  expect_equal(df$sq7[3], 1)
-  expect_equal(df$ndvi_max[4],.462)
-  expect_equal(df$geoID[9], 1063)
-  expect_equal(df$poly_id[10], 1063)
+#use sum to reliably check rainfall data
+test_that("monthly rainfall data is correct", {
+  sumrainfall= sum(df1$rainfall_month3)
+  expect_equal(sumrainfall, 56763.272)
+
 })
 
-#error checks
+#Step 5: run tests for expected output for annual output
+#testing if non-numeric variables are equal
+test_that("testing annual data is correctly merged", {
+  expect_equal(df2$ADM2_PCODE[61], "NG001014")
+  expect_equal(df2$ADM2_EN[627], "Ugwunagbo")
+  expect_equal(df2$ADM1_EN[6], "Abia")
+
+})
+
+#use sum to reliably check rainfall data
+test_that("annual rainfall data is correct", {
+  sumrainfall= sum(df2$rainfall_annual2)
+  expect_equal(sumrainfall, 10738505)
+
+})
+
+#Step 6: Create pathways for possible errors
+testthat_print("checking for expected output")
 test_that("code gives expected errors)", {
   #test if time_unit is null
   expect_error(geolink_chirps(time_unit = "null",
                               start_date = "2020-05-01",
                               end_date = "2020-02-01",
                               shp_dt = shp_dt[shp_dt$ADM1_PCODE == "NG001",],
-                              grid_size = 1000,
                               survey_dt = hhgeo_dt,
                               extract_fun = "mean"), "Time unit should either be month or annual", fixed= TRUE)
 
@@ -71,14 +70,13 @@ test_that("code gives expected errors)", {
                               start_date = "2020-05-01",
                               end_date = "2020-02-01",
                               shp_dt = shp_dt[shp_dt$ADM1_PCODE == "NG001",],
-                              grid_size = 1000,
                               survey_dt = hhgeo_dt,
                               extract_fun = "mean"), "Invalid time range, start time exceeds end time!", fixed= TRUE)
 
   #test if date is formatted wrongly as year/day/month
   expect_error(geolink_chirps(time_unit = "null",
-                              start_date = "2020-31-01",
-                              end_date = "2020-31-02",
+                              start_date = "2020-14-01",
+                              end_date = "2020-28-02",
                               shp_dt = shp_dt[shp_dt$ADM1_PCODE == "NG001",],
                               grid_size = 1000,
                               survey_dt = hhgeo_dt,
